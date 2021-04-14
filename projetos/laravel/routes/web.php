2a57ac58c8dc37a4ed84da6edf54118d7895049f
edsonisidoroinacio <eiinacio@yahoo.com.br>
2021-04-14 01:45:11 -0300

    Recuperando aulas composer

---
+++ projetos/laravel/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::view('/','welcome');
+Route::view('/teste','teste');
+
+Route::get('/noticia/{slug}', function($slug) {
+    echo "Slug: ".$slug;
+});
+
+Route::get('/noticia/{slug}/comentario/{id}', function($slug, $id) {
+    echo "MOSTRANDO O COMENTÁRIO".$id."DA NOTICIA".$slug;
+});
